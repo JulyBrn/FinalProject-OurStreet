12c2fbbc2d13e895de91f368017dd0a57caf0358
JulyBrn <julybrn60@gmail.com>
2020-04-28 19:35:40 +0200

    Add User Entity, create login and signin form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -9,21 +16,46 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/connexion", name="login")
+     * @Route("/login", name="login")
      */
-    public function login(AuthenticationUtils $authUtils)
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        $error = $authUtils->getLastAuthenticationError();
-        $lastName = $authUtils->getLastUsername();
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', [
-            'last_user' => $lastName,
+            'last_username' => $lastUsername,
             'error' => $error
         ]);
     }
-    
-     /**
-     * @Route("/deconnexion", name="logout")
+
+    /**
+     * @Route("/logout", name="logout")
      */
-    public function logout(){}
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/signIn", name="signIn")
+     */
+    public function register(Request $request, ObjectManager $manager)
+    {
+        $user = new User();
+        $form = $this->CreateForm(RegistrationFormType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($user);
+            $manager->flush();
+        }
+
+
+
+        return $this->render('security/signIn.html.twig', [
+            'form' => $form->createView()
+        ]);        
+    }
 }
