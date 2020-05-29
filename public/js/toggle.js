aa1a67ba6d3af2ca19484593618028dc7f6a1177
JulyBrn <julybrn60@gmail.com>
2020-05-29 18:43:51 +0200

    comment container

--- public/js/toggle.js
+++ public/js/toggle.js
@@ -2,9 +2,22 @@ const btn = document.getElementById("btn-comment");
 const formComment = document.getElementsByClassName("pushComment"); 
 
 let isVisible = false;
-console.log(isVisible);
 
 btn.addEventListener('click',() => {
     isVisible = !isVisible;
-    isVisible ? formComment[0].classList.add('is-visible') : formComment[0].classList.remove('is-visible') 
+    if (isVisible){
+        formComment[0].classList.remove('slideUp');
+        formComment[0].classList.add('is-visible');
+        formComment[0].classList.add('slideDown');
+        setTimeout(()=>{
+            formComment[0].classList.remove('slideDown');
+        }, 1000);
+        btn.textContent = "Cacher";
+    } else{
+        formComment[0].classList.add('slideUp');
+        setTimeout(()=>{
+            formComment[0].classList.remove('is-visible');
+        }, 1000);
+        btn.textContent = "Commenter";
+    }
 });
